e99d88b9955d5d94e81bec14d9aa98c63d2ee03a
6huGt <jmsnars@gmail.com>
2024-09-27 17:23:39 -0400

    Update main.js

--- config/main.js
+++ config/main.js
@@ -1,7 +1,7 @@
 // This changes the title of your site
 
-var sitename = "native."; // Change this to change the name of your website.
-var subtext = "v1.2"; // set the subtext
+var sitename = "Space."; // Change this to change the name of your website.
+var subtext = "v9.0"; // set the subtext
 
 // more settings in main.css
 
